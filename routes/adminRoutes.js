3aa314703c3bab7a90c0640bb109e3d8fa6de0d4
hornto <krishnadas10.official@gmail.com>
2023-10-30 23:19:43 +0530

    first commit

---
+++ routes/adminRoutes.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/AdminController");
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const tryCatch=require("../middileware/TryCatchMiddileware")
+const verifyToken=require("../middileware/authMiddleware")
+
+
+router.use(express.json());
+
+router.post("/register", tryCatch(controller.register));
+router.post("/login", tryCatch(controller.login));
+router.post("/users", verifyToken, upload.single('photo'), tryCatch(controller.createuser));
+router.get("/users", verifyToken, tryCatch(controller.getallusers));
+router.get("/users/:id", verifyToken, tryCatch(controller.getuserByid));
+router.put("/users/:id", verifyToken, tryCatch(controller.updateuserByid));
+router.delete("/users/:id", verifyToken, tryCatch(controller.deleteuserByid));
+
+
+
+module.exports = router;
